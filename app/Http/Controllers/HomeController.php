e14c053f9424f04d8d13413517ed24cc3a6d905c
zuhalcode <zuhalcode@gmail.com>
2024-06-23 12:03:51 +0700

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pencatatan;
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $lateCount = Pencatatan::where('p_piutang', 1)->count();
-        $notLateCount = Pencatatan::where('p_piutang', 0)->count();
+        $lateCount = Pencatatan::where('prediction', 1)->count();
+        $notLateCount = Pencatatan::where('prediction', 0)->count();
         return view('pencatatan.dashboard', ["late" => $lateCount, 'notLate' => $notLateCount]);
     }
 }
